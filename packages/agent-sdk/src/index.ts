7da0a0552494c42d2b1bddfda3922158aa739ff9
Nikhil Sonti <nikhilsv92@gmail.com>
2026-01-01 11:07:06 -0800

    feat: agent-sdk outline

---
+++ packages/agent-sdk/src/index.ts
@@ -0,0 +1,24 @@
+export { Agent } from './agent.js'
+export {
+  ActionError,
+  AgentSDKError,
+  ConnectionError,
+  ExtractionError,
+  NavigationError,
+  VerificationError,
+} from './errors.js'
+export type {
+  ActOptions,
+  ActResult,
+  ActStep,
+  AgentOptions,
+  ExtractOptions,
+  ExtractResult,
+  NavOptions,
+  NavResult,
+  ProgressEvent,
+  ProgressEventType,
+  ToolCall,
+  VerifyOptions,
+  VerifyResult,
+} from './types.js'
