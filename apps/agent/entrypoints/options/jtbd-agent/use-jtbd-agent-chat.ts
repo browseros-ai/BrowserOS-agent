d7dd72a8d6a6da48f34a772edfcf52729a284ca5
Felarof <nithin.sonti@gmail.com>
2026-01-10 11:11:08 -0800

    fix: url

--- apps/agent/entrypoints/options/jtbd-agent/use-jtbd-agent-chat.ts
+++ apps/agent/entrypoints/options/jtbd-agent/use-jtbd-agent-chat.ts
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 import { getBrowserOSAdapter } from '@/lib/browseros/adapter'
 import { BROWSEROS_PREFS } from '@/lib/browseros/prefs'
 
-const JTBD_API_URL = 'http://localhost:3001' // 'https://jtbd-agent.fly.dev'
+const JTBD_API_URL = 'https://jtbd-agent.fly.dev' // 'http://localhost:3001'
 const EXPERIMENT_ID = 'jtbd_jan26'
 
 async function getInstallId(): Promise<string> {
